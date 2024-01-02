c85814ca10340504b66d25c56c5136a92c0d4c14
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-01-02 09:30:32 +0300

    Add project files.

---
+++ TaskFromApp/Form1.Designer.cs
@@ -0,0 +1,96 @@
+﻿namespace TaskFromApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnReadFile = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.BtnCounter = new System.Windows.Forms.Button();
+            this.textBoxCounter = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // BtnReadFile
+            // 
+            this.BtnReadFile.Location = new System.Drawing.Point(12, 42);
+            this.BtnReadFile.Name = "BtnReadFile";
+            this.BtnReadFile.Size = new System.Drawing.Size(75, 23);
+            this.BtnReadFile.TabIndex = 0;
+            this.BtnReadFile.Text = "Dosya Oku";
+            this.BtnReadFile.UseVisualStyleBackColor = true;
+            this.BtnReadFile.Click += new System.EventHandler(this.BtnReadFile_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 89);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(100, 96);
+            this.richTextBox1.TabIndex = 1;
+            this.richTextBox1.Text = "";
+            // 
+            // BtnCounter
+            // 
+            this.BtnCounter.Location = new System.Drawing.Point(268, 42);
+            this.BtnCounter.Name = "BtnCounter";
+            this.BtnCounter.Size = new System.Drawing.Size(75, 23);
+            this.BtnCounter.TabIndex = 2;
+            this.BtnCounter.Text = "Sayaç Arttır";
+            this.BtnCounter.UseVisualStyleBackColor = true;
+            this.BtnCounter.Click += new System.EventHandler(this.BtnCounter_Click);
+            // 
+            // textBoxCounter
+            // 
+            this.textBoxCounter.Location = new System.Drawing.Point(268, 89);
+            this.textBoxCounter.Name = "textBoxCounter";
+            this.textBoxCounter.Size = new System.Drawing.Size(100, 20);
+            this.textBoxCounter.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.textBoxCounter);
+            this.Controls.Add(this.BtnCounter);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.BtnReadFile);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnReadFile;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Button BtnCounter;
+        private System.Windows.Forms.TextBox textBoxCounter;
+    }
+}
+
---
+++ TaskFromApp/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TaskFromApp
+{
+    public partial class Form1 : Form
+    {
+        public int counter { get; set; } = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void BtnReadFile_Click(object sender, EventArgs e)
+        {
+            //string data=ReadFile();
+            string data=await ReadFileAsync();
+            richTextBox1.Text = data;
+        }
+
+        private void BtnCounter_Click(object sender, EventArgs e)
+        {
+            textBoxCounter.Text =counter++.ToString();
+        }
+
+        private string ReadFile()
+        {
+            string data=string.Empty;
+            using (StreamReader s = new StreamReader("dosya.txt"))
+            {
+                //Thread.Sleep(5000);
+                data = s.ReadToEnd();
+            }
+            return data;
+        }
+        private async Task<string> ReadFileAsync()
+        {
+            string data = string.Empty;
+            using (StreamReader s = new StreamReader("dosya.txt"))
+            {
+                //Thread.Sleep(5000);
+                data = await s.ReadToEndAsync(); 
+                 
+                await Task.Delay(4000);
+                return data;
+            }
+           
+        }
+    }
+}
