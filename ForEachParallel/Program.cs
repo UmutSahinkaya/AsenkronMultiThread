02a8545336902dc1a13232b9ea7bfdf42ae52b34
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-01-02 14:26:56 +0300

    TPL parallel.Foreach RaceCondition

---
+++ ForEachParallel/Program.cs
@@ -0,0 +1,75 @@
+﻿using System.Diagnostics;
+using System.Drawing;
+
+namespace ForEachParallel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            long FilesByte = 0;
+            int deger=0;
+            #region ParallelForeach    //İşlem 00:00:00.1861995 sürede Tamamlandı !
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            //string picturePath = @"C:\Users\Umut.Sahinkaya\Desktop\Pictures";
+
+            //var files = Directory.GetFiles(picturePath);
+
+            //Parallel.ForEach(files, (item) =>
+            //{
+            //    Console.WriteLine("Thread No: " + Thread.CurrentThread.ManagedThreadId);
+            //    Image img = new Bitmap(item);
+
+            //    var thumbnail = img.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
+
+            //    thumbnail.Save(Path.Combine(picturePath, "thumbnail", Path.GetFileName(item)));
+            //});
+            //sw.Stop();
+            //Console.WriteLine($"İşlem{sw} sürede Tamamlandı !");//İşlem00:00:00.1861995 sürede Tamamlandı !
+            #endregion
+            #region NormalForeach     //İşlem 00:00:00.4351508 sürede Tamamlandı !
+            //Stopwatch sw = new Stopwatch();
+            //sw.Start();
+            //string picturePath = @"C:\Users\Umut.Sahinkaya\Desktop\Pictures";
+
+            //var files = Directory.GetFiles(picturePath);
+
+            //files.ToList().ForEach(x =>
+            //{
+            //    Console.WriteLine("Thread No: " + Thread.CurrentThread.ManagedThreadId);
+            //    Image img = new Bitmap(x);
+
+            //    var thumbnail = img.GetThumbnailImage(50, 50, () => false, IntPtr.Zero);
+
+            //    thumbnail.Save(Path.Combine(picturePath, "thumbnail", Path.GetFileName(x)));
+            //});
+            //sw.Stop();
+            //Console.WriteLine($"İşlem{sw} sürede Tamamlandı !");//İşlem00:00:00.4351508 sürede Tamamlandı !
+            #endregion
+            #region ParallelForeach Race Condition Engelleme  //Toplam boyut: 4626340
+
+            //string picturePath = @"C:\Users\Umut.Sahinkaya\Desktop\Pictures";
+
+            //var files = Directory.GetFiles(picturePath);
+
+            //Parallel.ForEach(files, (item) =>
+            //{
+            //    Console.WriteLine("Thread No: " + Thread.CurrentThread.ManagedThreadId);
+            //    FileInfo f = new FileInfo(item);
+            //    Interlocked.Add(ref FilesByte, f.Length);
+            //});
+            
+            //Console.WriteLine("Toplam boyut: {0}",FilesByte.ToString());
+            #endregion
+            #region RaceCondition Ornek
+            //Parallel.ForEach(Enumerable.Range(1, 100000).ToList(), (x) =>
+            //{
+            //    deger = x;
+
+            //});
+            //Console.WriteLine("Değer:" + deger); Bu haliyle alıyor.
+            #endregion
+        }
+    }
+}
---
+++ TaskConsoleApp/Program.cs
@@ -0,0 +1,55 @@
+﻿public class Content
+{
+    public string Site { get; set; }
+    public int Len { get; set; }
+}
+internal class Program
+{
+    private async static Task Main(string[] args)
+    {
+        Console.WriteLine("Main Thread:" + Thread.CurrentThread.ManagedThreadId);
+
+        List<string> urls = new List<string>()
+        {
+            "https://www.google.com",
+            "https://www.microsoft.com",
+            "https://www.amazon.com",
+            "https://www.n11.com",
+            "https://www.haberturk.com"
+        };
+        List<Task<Content>> taskList = new List<Task<Content>>();
+
+        urls.ToList().ForEach(x =>
+        {
+            taskList.Add(GetContentAsync(x));
+        });
+        #region WhenAny
+        var FirstData =await Task.WhenAny(taskList);
+
+        Console.WriteLine($"{FirstData.Result.Site} -  {FirstData.Result.Len}");
+        #endregion
+        #region WhenAll
+        //var contents = Task.WhenAll(taskList.ToArray());
+
+        ////İşlemler olurken arada başka kodlar çalıştırmak istiyorsak bu şekilde de çalışabiliriz.
+        //Console.WriteLine("I did other something after WhenAll");
+
+        //var data =await contents;
+
+        //data.ToList().ForEach(x =>
+        //{
+        //    Console.WriteLine($"{x.Site} boyut: {x.Len}");
+        //});
+        #endregion
+    }
+
+    public static async Task<Content> GetContentAsync(string url)
+    {
+        Content c = new Content();
+        var data = await new HttpClient().GetStringAsync(url);
+        c.Site = url;
+        c.Len = data.Length;
+        Console.WriteLine("GetContentAsync thread:" + Thread.CurrentThread.ManagedThreadId);
+        return c;
+    }
+}
