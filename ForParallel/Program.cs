7b50056aff37b9c77c95b186da207b68fdc8632f
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-01-03 09:53:44 +0300

    TPL ForForEach

---
+++ ForParallel/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace ForParallel
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Parallel.For
+            //long totalByte = 0;
+
+            //var files = Directory.GetFiles(@"C:\Users\Umut.Sahinkaya\Desktop\Pictures");
+            //Parallel.For(0, files.Length, (index) =>
+            //{
+            //    Console.WriteLine("Thread No:" + Thread.CurrentThread.ManagedThreadId);
+            //    var file = new FileInfo(files[index]);
+            //    Interlocked.Add(ref totalByte, file.Length);
+            //});
+
+            //Console.WriteLine("Total Byte: " + totalByte.ToString());
+            #endregion
+
+            #region Parallel.ForEach SharedData
+            //int total = 0;
+            //Parallel.ForEach(Enumerable.Range(1, 100).ToList(), () => 0, (x, loop, subTotal) =>
+            //{
+            //    subTotal += x;
+            //    return subTotal;
+            //},(y)=>Interlocked.Add(ref total,y));
+            //Console.WriteLine(total);
+            #endregion
+            #region Parallel.For SharedData
+            int total = 0;
+            Parallel.For(0, 100, () => 0, (x, loop, subTotal) =>
+            {
+                subTotal += x;
+                return subTotal;
+            }, (y) => Interlocked.Add(ref total, y));
+            Console.WriteLine(total);
+            #endregion
+        }
+    }
+}
