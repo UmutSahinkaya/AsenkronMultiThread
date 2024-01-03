7b241410517db6aff80f984f07677b33b94750fd
umutSahinkaya <umutsahinkaya1@gmail.com>
2024-01-03 15:36:54 +0300

    veri tabanı bağlandı ve verilerden 10 tanesi getirildi

---
+++ PLINQ.EFCore/Program.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using static System.Net.Mime.MediaTypeNames;
+using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+using PLINQ.EFCore.Models;
+
+namespace PLINQ.EFCore
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            AdventureWorks2017Context _context = new AdventureWorks2017Context();
+            _context.Products.Take(10).ToList().ForEach(x =>
+            {
+                Console.WriteLine(x.Name);
+            });
+        }
+    }
+}
+
